6a55c70aa40404175d74086b34aac2f91ee7c133
wtnbmtk <wtnbmtk7@gmail.com>
2023-05-22 04:59:40 +0900

    commit

--- general.js
+++ general.js
@@ -1,38 +1,43 @@
-// Nav Fixed
+const header = document.getElementById("TopHeader");
+const Button = document.getElementById("ToggleButton");
+
+Button.addEventListener("click", function () {
+  if (Button.innerHTML === "MENU") {
+    header.classList.add("open");
+    Button.innerHTML = "CLOSE";
+  } else {
+    header.classList.remove("open");
+    Button.innerHTML = "MENU";
+  }
+});
+
 window.addEventListener("scroll", function () {
-  const header = document.getElementById("TopHeader");
-  if (window.scrollY > header.offsetHeight) {
+  if (window.pageYOffset > 50) {
     header.classList.add("fixed");
   } else {
     header.classList.remove("fixed");
   }
 });
 
-// Nav Toggle Button
-const trigger = document.getElementById("nav-toggle");
-if (trigger) {
-  trigger.addEventListener(
-    "click",
-    function () {
-      let header = document.getElementById("TopHeader");
-      header.classList.toggle("open");
-    },
-    false
-  );
-}
-
 window.addEventListener("resize", function () {
-  const x = 800;
-  const target = document.getElementById("TopHeader");
+  let x = 800;
   if (window.innerWidth >= x) {
-    target.classList.remove("open");
+    header.classList.remove("open");
+    Button.innerHTML = "MENU";
   }
 });
 
 const anchorTags = document.querySelectorAll('a[href*="#"]');
 anchorTags.forEach(function (anchorTag) {
   anchorTag.addEventListener("click", function () {
-    const drawer = document.getElementById("TopHeader");
-    drawer.classList.remove("open");
+    header.classList.remove("open");
+    Button.innerHTML = "MENU";
   });
 });
+
+document.querySelector("iframe").src = document.querySelector("iframe").src;
+const iframe =
+  document.querySelector("iframe").contentDocument.documentElement.innerHTML;
+console.log(iframe);
+const element = iframe.querySelector("body");
+console.log(element);
